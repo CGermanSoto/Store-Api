7b2c0fa189811c8441dcef65fe05c8e3ecfe280e
CGermanSoto <c.germansoto@gmail.com>
2022-03-30 12:06:54 -0300

    DB & SERVER runing

---
+++ Middleware/error-handler.js
@@ -0,0 +1,5 @@
+const errorHandlerMiddleware = async(err,req,res,next) =>{
+    console.log(err)
+}
+
+module.exports = errorHandlerMiddleware
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config() //DOTENV
+const express = require('express')// Express
+const app = express()// Express
+const connectDB = require('./db/connect') //Database
 
+const notFoundMiddleware = require('./Middleware/not-found')// Middleware error
+const errorMiddleware = require('./Middleware/error-handler')// Middleware error
+
+//Middleware 
+app.use(express.json())
+
+// rootes
 app.get('/', (req,res)=>{
-    res.send('En construcción')
+    res.send('<h1>Store Api</h1><a href="/api/v1/products">products route</a>')
 })
 
-const PORT = process.env.PORT
+//errors
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
-app.listen(PORT, ()=>{
-    console.log(`Conectado en puerto ${PORT}`)
-})
+// Connect server & DB
+const port = process.env.PORT
+
+const start = async()=>{
+    try {   
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server y DB corriendo en puerto ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
